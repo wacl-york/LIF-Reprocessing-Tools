15990c8ab4bc4a74746aa9b349ef8edcdcc61d9e
Phin-Petherick <phin.petherick@york.ac.uk>
2025-11-12 14:48:16 +0000

    misaligned files function
    added misaligned files function as a separate file to test

--- src/LifPy/misaligned_files.py
+++ src/LifPy/misaligned_files.py
@@ -22,21 +22,21 @@ channel_format ={
 
 day_folders = lif.find_day_folders(data_dir)
 
-def misaligned_counts(data_dir, day_folders, channels_to_use):
+def misaligned_counts(data_dir, day_folders, channels_to_use, cal_task=2):
 
     HK_data = lif.import_HK_data(data_dir, day_folders)
-    processing_variables = pd.read_csv(os.path.join(data_dir, 'processing_variables.txt'))
-    soft_restarts = pd.read_csv(os.path.join(data_dir, 'soft_restarts.txt'))
+    processing_variables_file_path = os.path.join(data_dir, 'processing_variables.txt')
+    processing_variables = pd.read_csv(processing_variables_file_path).copy()
+    soft_restarts = pd.read_csv(os.path.join(data_dir, 'soft_restarts.txt')).copy()
     
     for channel in channels_to_use:
-        processing_variables[f'{channel}_shift'] = 0
         soft_restarts[f'{channel}_shift'] = 0
         
 
     # Loop through each soft restart 
     for i in soft_restarts.index:
         
-        print(f"\nchecking for misaligned data in file: {soft_restarts['bin_filename'][i]}")
+        print(f"\rchecking for misaligned data in file: {soft_restarts['bin_filename'][i]}", end='')
         
         log_start_datetime_seconds = lif.format_log_start_datetime(soft_restarts['log_start_datetime'][i])
         bin_data = lif.import_bin_data(data_dir, str(soft_restarts['date'][i]), str(soft_restarts['bin_filename'][i]))
@@ -78,18 +78,62 @@ def misaligned_counts(data_dir, day_folders, channels_to_use):
         # Note: Since HK_data['Task'] is a NumPy array, we index it directly.
         data_df['Task'] = HK_data['Task'][HK_ind_absolute]
         
-        ### DO THE FILE MISALIGNMENT CALCULATION ### 
+        test_section = data_df.loc[data_df['Task'] == cal_task].iloc[2200:4200]
         
-        ### APPEND THE FILE SHIFT VALUES TO THE SOFT RESTARTS FILE ###
+        for channel in channels_to_use:
+        # Container for sum of stds        
+            sd_results = []
+           
+            # For each lag (0, 1, -1)
+            #these are the defined shifts. These may change.
+            for lag in [0, -1, 1]:
+                #simply makes the value 0 and then we add onto this
+                sum_of_sds = 0
+           
+                # For each mode
+                #offline ==5
+                #online ==6
+                for mode in [5, 6]:
+                   
+                    # Get raw data
+                    # using the above to do the shifts on the data by what seed ld mode we have and the
+                    #channels specified from channel data
+                    values = test_section.loc[test_section['seed_LD_mode'] == mode, channel]
+                    values_lagged = values.shift(lag)
+                    # Calculate mean & sd
+                    #add it on to the 0 from the sum_of_stds and then also the new calulated values
+                    sum_of_sds += values_lagged.std()
+               
+                # Save sum of sds for this lag
+                sd_results.append((sum_of_sds, lag))
+               
+            # Find lag with minimum sum of sds. NB: min() of a list of tuples sorts by
+            # first tuple entry, i.e. sd.
+            best_lag = min(sd_results)[1]
+            
+            soft_restarts.loc[f'{channel}_shift'] = best_lag        
         
-    ### TRANSFER THE FILE SHIFT VALUES FROM THE SOFT RESTARTS FILE TO ALL CORRESPONDING LINES IN PROCESSING VARIABLES ###
-    ### OVERWRITE THE PROCESSING VARIABLES FILE WITH THE SHIFTS APPENDED ### 
 
+    # merge the shift values onto the processing variables df based on restart index
+    shift_columns  = [f'{channel}_shift' for channel in channels_to_use]
+    columns_to_merge = shift_columns + ['restart_index']
 
+    processing_variables_shifts = pd.merge(
+        processing_variables
+        , soft_restarts[columns_to_merge]
+        , on='restart_index'
+        , how='left'               
+    )
+    
+    processing_variables_shifts[shift_columns] = processing_variables_shifts[shift_columns].fillna(0)
+    processing_variables_shifts = processing_variables_shifts.drop(columns=['restart_index'])
 
+    ### Overwrite the processing variables file with the shifts appended
+    processing_variables_shifts.to_csv(processing_variables_file_path, index=False)
+    print('\nProcessing variables file updated to include shift values')
 
         
-test_data, bin_data_dict = misaligned_counts(data_dir, day_folders, channels_to_use = ["sig_A_counts", "sig_B_counts", "ref_counts", "seed_LD_current"])
+misaligned_counts(data_dir, day_folders, channels_to_use = ["sig_A_counts", "sig_B_counts", "ref_counts", "seed_LD_current"])
             
                 
                 
